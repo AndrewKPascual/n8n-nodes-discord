2670f7ae55bf9e68689877d9805bd423198cf0eb
AndrewKPascual <95746939+AndrewKPascual@users.noreply.github.com>
2023-07-06 20:26:14 -0700

    test

--- nodes/Discord/bot/ipcEvents/credentials.ipc.ts
+++ nodes/Discord/bot/ipcEvents/credentials.ipc.ts
@@ -1,11 +1,11 @@
 import Ipc from 'node-ipc';
 import { Client } from 'discord.js';
-import { ICredentials, addLog, withTimeout  } from '../helpers';
+import { ICredentials, addLog, withTimeout } from '../helpers';
 import state from '../state';
 import commands from '../commands';
 
 export default async function (ipc: typeof Ipc, client: Client) {
-  ipc.server.on('credentials', (data: ICredentials, socket: any) => {
+  ipc.server.on('credentials', async (data: ICredentials, socket: any) => {
     try {
       addLog(`credentials state login ${state.login}, ready ${state.ready}`, client);
       if (
@@ -13,27 +13,23 @@ export default async function (ipc: typeof Ipc, client: Client) {
         (state.ready && (state.clientId !== data.clientId || state.token !== data.token))
       ) {
         if (data.token && data.clientId) {
-					addLog(`credentials login authenticating`, client);
+          addLog(`credentials login authenticating`, client);
           state.login = true;
           client.destroy();
-          commands(data.token, data.clientId, client).catch((e) => {
-            addLog(`${e}`, client);
-          });
-          withTimeout(client
-            .login(data.token), 3000)
-            .then(() => {
-              state.ready = true;
-              state.login = false;
-              state.clientId = data.clientId;
-              state.token = data.token;
-              ipc.server.emit(socket, 'credentials', 'ready');
-              addLog(`credentials ready`, client);
-            })
-            .catch((e) => {
-              state.login = false;
-              ipc.server.emit(socket, 'credentials', 'error');
-              addLog(`credentials error`, client);
-            });
+          try {
+            await commands(data.token, data.clientId, client);
+            await withTimeout(client.login(data.token), 3000);
+            state.ready = true;
+            state.login = false;
+            state.clientId = data.clientId;
+            state.token = data.token;
+            ipc.server.emit(socket, 'credentials', 'ready');
+            addLog(`credentials ready`, client);
+          } catch (e) {
+            state.login = false;
+            ipc.server.emit(socket, 'credentials', 'error');
+            addLog(`credentials error ${e}`, client);
+          }
         } else {
           ipc.server.emit(socket, 'credentials', 'missing');
           addLog(`credentials missing`, client);
